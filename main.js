c0995c795882f7b6a7c7669c798749f41488ccd4
menon-14 <mariana.menon@escola.pr.gov.br>
2026-06-25 08:35:57 -0300

    wert

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent - tamanhoSenha;
 
-numeroSenha.textContent - 5;
+const botoes - document.querySelectorA11('.parametro-senha__botao');
+
+botoes[0].onclik - diminuiTamanho;
+
+function diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha-1
+}
+console.log(botoes)
