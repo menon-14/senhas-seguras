659519e40261d02b81bd1a3b43dbe23386ee5aaa
menon-14 <mariana.menon@escola.pr.gov.br>
2026-06-25 08:46:14 -0300

    ferth

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-numeroSenha.textContent - tamanhoSenha;
+numeroSenha.textContent = tamanhoSenha;
 
-const botoes - document.querySelectorA11('.parametro-senha__botao');
+const botoes = document.querySelectorA11('.parametro-senha__botao');
 
-botoes[0].onclik - diminuiTamanho;
+botoes[0].onclik = diminuiTamanho;
+botoes[1].onclik = aumantaTamanho;
 
 function diminuiTamanho(){
     tamanhoSenha = tamanhoSenha-1
+    numeroSenha.textContent = tamanhoSenha;
 }
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha+1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
 console.log(botoes)
