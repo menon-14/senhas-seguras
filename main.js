f432ac8de73fa1b317f2e70ca850e9ca78e27570
menon-14 <mariana.menon@escola.pr.gov.br>
2026-06-25 12:56:56 -0300

    edrfgthy

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorA11('.parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-botoes[0].onclik = diminuiTamanho;
-botoes[1].onclik = aumantaTamanho;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if (tamanhoSenha > 1){ 
-    tamanhoSenha = tamanhoSenha-1
-    }
-    numeroSenha.textContent = tamanhoSenha;
-}
-function aumentaTamanho(){
-    if (tamanhoSenha < 20){
-        tamanhoSenha = tamanhoSenha+1;
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
 
-console.log(botoes)
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+console.log(botoes);
