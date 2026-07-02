b3d55c6a351480af6e0f48ad524d7b71965ef34b
menon-14 <mariana.menon@escola.pr.gov.br>
2026-07-02 08:39:45 -0300

    tyhhr

--- main.js
+++ main.js
@@ -21,4 +21,18 @@ function aumentaTamanho() {
     numeroSenha.textContent = tamanhoSenha;
 }
 
-console.log(botoes);
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+gerarSenha();
+
+function gerarSenha(){
+
+    for (let i = 0; i < tamanhoSenha;i++){ 
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        console.log(letrasMaiusculas[numeroAleatorio]); 
+    }
+}
+
+campoSenha.value =  letrasMaiusculas;
